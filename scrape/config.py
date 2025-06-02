a29746efbc679505a95b9996f63ae0f334022bc4
Amar Munoglu <ammarmunoglu@gmail.com>
2025-06-02 15:25:02 +0200

    Removed/Changed files; Ready for main branch

--- scrape/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Grok3 API Configuration
-# IMPORTANT: Set these as environment variables on your system!
-GROK3_ANONUSERID = os.getenv("GROK3_ANONUSERID")
-GROK3_CHALLENGE = os.getenv("GROK3_CHALLENGE")
-GROK3_SIGNATURE = os.getenv("GROK3_SIGNATURE")
-GROK3_SSO = os.getenv("GROK3_SSO")
-GROK3_SSO_RW = os.getenv("GROK3_SSO_RW")
